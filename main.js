219e164074a3dab9f5f3dd08dafb614c6151d766
johnlin <john830110@gmail.com>
2023-04-23 17:02:25 +0800

    test: 修改 console.log 內容

--- main.js
+++ main.js
@@ -4,5 +4,5 @@ dotenv.config({path:"./.env"});
 
 console.log(process.env.TEST)
 if (process.env.TEST === "testing") console.log("It is testing")
-else if (process.env.TEST === undefined) console.log("It is undefined")
+else if (process.env.TEST === undefined) console.log("It should be undefined")
 else console.log("I don't know")
