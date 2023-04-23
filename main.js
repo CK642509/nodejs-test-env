1d1928f677ab5c4484da69f297697202e656f66f
johnlin <john830110@gmail.com>
2023-04-23 16:59:03 +0800

    test: 修改 console.log 內容

--- main.js
+++ main.js
@@ -3,3 +3,6 @@ const dotenv = require('dotenv');
 dotenv.config({path:"./.env"});
 
 console.log(process.env.TEST)
+if (process.env.TEST === "testing") console.log("It is testing")
+else if (process.env.TEST === undefined) console.log("It is undefined")
+else console.log("I don't know")
